59ab37311ced94c739096eaf586f2229adb3271c
Brian Williams <brn.j.williams@gmail.com>
2015-03-30 18:35:02 -0500

    update piv scripts

--- imagej/processPIVTemplate.js
+++ imagej/processPIVTemplate.js
@@ -1,36 +1,74 @@
 importClass(Packages.ij.IJ);
 importClass(Packages.ij.ImagePlus);
 importClass(Packages.ij.ImageStack);
+importClass(Packages.ij.process.ImageProcessor);
+importClass(Packages.ij.gui.Toolbar);
+importClass(Packages.ij.WindowManager);
+importClass(Packages.ij.plugin.frame.RoiManager);
+importClass(Packages.ij.gui.GenericDialog);
 
+side = 0;
+piv1 = 64;
+sw1 = 128;
+vs1 = 32;
+piv2 = 32;
+sw2 = 64;
+vs2 = 16;
+piv3 = 0;
+sw3 = 0;
+vs3 = 0;
+corr = 0.8;
+path = "/home/brian/working/pivOutput/";
 
 // Following line should be built by bash script:
-stem = 
 
+imp = IJ.openImage("/home/brian/working/sampleStack.tif")
+IJ.log("opened image");
+imp.show();
 
-str1 = "open=./";
-str2 = "/spool0000000000.dat image=[16-bit Unsigned] width=";
-str3 = " height=";
-str4 = " offset=0 number=1 gap=0 little-endian open";
-
-str = str1 + stem + str2 + width + str3 + height + str4;
-
-IJ.log(str);
-
-IJ.run("Raw...", str);
-
-//IJ.run("Raw...", "open=/home/brian/data3/projects/cardiac/20150217_bridgeExp/20150219_b3_CHS_528_10x_50fps_16bit/spool0000000000.dat image=[16-bit Unsigned] width=528 height=512 ofsset=0 number=1 gap=0 little-endian open");
-
-imp = IJ.getImage();
-
-IJ.log("Preparing to save");
-
-IJ.saveAs("Tiff", "/mnt/ssd/output.tiff");
-IJ.saveAs("Tiff", "./" + stem + ".tiff");
+//i0 = WindowManager.getCurrentWindow();
+IJ.run("StackReg", "transformation=[Rigid Body]");
+IJ.saveAs("Tiff", "/home/brian/working/sampleStack_bgs.tif");
 
 IJ.log("Video saved");
 
+var stackSize = imp.getImageStackSize();
+IJ.log("Slices: " + stackSize);
+changes = false;
+
+stack = imp.getStack();
+for(s=1;s<stackSize;s++){
+    var ss = s+1;
+
+    stack2 = ImageStack(stack.getWidth(), stack.getHeight(), stack.getColorModel());
+    ip = stack.getProcessor(s);
+
+    ip2 = ip.createProcessor(stack.getWidth(), stack.getHeight());
+    ip2.insert(stack.getProcessor(s),0,0);
+    stack2.addSlice(null, ip2);
+
+    ip2 = ip.createProcessor(stack.getWidth(), stack.getHeight());
+    ip2.insert(stack.getProcessor(ss),0,0);
+    stack2.addSlice(null, ip2);
+
+    imp2 = ImagePlus("current", stack2);
+    imp2.show();
+
+    IJ.log("s = " + s);
+    
+    //IJ.run("iterative PIV(Advanced)...", " piv1="+piv1+" sw1="+sw1+" vs1="+vs1+" piv2="+piv2+" sw2="+sw2+"   vs2="+vs2+" piv3="+piv3+" sw3="+sw3+" vs3="+vs3+" correlation="+corr+" batch path=["+path+"]");
+
+    IJ.run("iterative PIV(Advanced)...", " piv1=64 sw1=128 vs1=32 piv2=32 sw2=64 vs2=16 piv3=0 correlation=0.8 batch path=[/home/brian/working/pivOutput/]");
 
 
-// Don't include following lines!!!
-width="528";
-height="512";
+    
+    imp2.changes = false;
+    imp2.close();
+
+}
+
+
+imp.close();
+
+
+
